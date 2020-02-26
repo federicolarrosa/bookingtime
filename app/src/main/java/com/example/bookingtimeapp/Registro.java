d6622f2d60ae5b7ee79971c5ebb72f3532ca8966
Federico Gabriel Larrosa <federico.gabriel.larrosa@gmail.com>
2020-02-25 23:11:48 -0300

    version 1

---
+++ app/src/main/java/com/example/bookingtimeapp/Registro.java
@@ -0,0 +1,18 @@
+package com.example.bookingtimeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.bookingtimeapp.ui.login.LoginActivity;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+           }
+}
